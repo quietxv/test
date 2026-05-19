9026dadb70ff776fe3d16315413cd27c535585cc
quiethub4 <Madindun>
2026-05-19 18:48:11 +0700

    update config.js

--- settings/config.js
+++ settings/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  tokenBot: "8560763603:AAHOYqyblb2wJ6-KAkDl_R-szPuzrMWUJV0",
+  tokenBot: "8906893566:AAFc86jIeBNawjnebvwbfEOEG1RztnnG1x0",
   ownerID: "6376186918",
   requiredChannel: "@VogueOfficialx",
   LOG_QUEUE_CHANNEL_ID: "@LogVagues"
